f86b0beb1ff0ccfe589ff08bd74d23d14cf4c386
vaibhavsoshte <vaibhavsoshte@gmail.com>
2023-01-19 14:11:35 +0530

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('studenttbl')->insert(array(
+            array(
+            'name' => "Steve",
+            'rollno' => '1',
+            'standard' => '8',
+            ),
+            array(
+            'name' => "Laura",
+            'rollno' => '2',
+            'standard' => '8',
+            )
+            ));
+            
     }
 }
